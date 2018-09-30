25f7a60be6db40a0bcb17d8075c8ce704c560ea6
HomerChenX1 <homerchenx1@gmail.com>
2018-09-30 18:36:23 +0800

    add autocompletetext finish

--- app/src/main/java/com/homer/linefake/MainActivity.java
+++ app/src/main/java/com/homer/linefake/MainActivity.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
@@ -13,12 +15,12 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private ProgressBar vProgress;
-    private TextInputLayout vEmailLayout;
-    private EditText vEmail;
-    private TextInputLayout vPasswordLayout;
+    // private TextInputLayout vEmailLayout; not necessary
+    private AutoCompleteTextView vEmail;
+    // private TextInputLayout vPasswordLayout; not necessary
     private EditText vPassword;
-    private Button vEmailSignInBtn;
-    private Button vRegisterBtn;
+    // private Button vEmailSignInBtn;  not necessary
+    // private Button vRegisterBtn;  not necessary
     private TextView vMessages;
     private String[] eMailAutoList ;
 
@@ -26,28 +28,45 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViews();
         setTitle(getString(R.string.app_name) + ": Login");
         eMailAutoList = getResources().getStringArray(R.array.email_auto_list);
+        findViews();
         vMessages.setText(eMailAutoList[0]+"\n"+eMailAutoList[1]+"\n"+eMailAutoList[2]+"\n"+eMailAutoList[3]);
     }
     private void findViews() {
         vProgress = findViewById(R.id.login_progress);
 
-        vEmailLayout = findViewById(R.id.login_email_layout);
+        // vEmailLayout = findViewById(R.id.login_email_layout);
         vEmail = findViewById(R.id.login_email);
+        // special adapter case : android.R.layout.simple_dropdown_item_1line, else will use examples/ch05
+        ArrayAdapter<String> adapter =
+                new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, eMailAutoList);
+        vEmail.setAdapter(adapter);
 
-        vPasswordLayout = findViewById(R.id.login_password_layout);
+        // vPasswordLayout = findViewById(R.id.login_password_layout);
         vPassword = findViewById(R.id.login_password);
 
-        vEmailSignInBtn = findViewById(R.id.login_email_sign_in_btn);
-        vRegisterBtn = findViewById(R.id.login_register_btn);
+        // vEmailSignInBtn = findViewById(R.id.login_email_sign_in_btn);
+        // vRegisterBtn = findViewById(R.id.login_register_btn);
         vMessages = findViewById(R.id.login_messages);
     }
 
     public void onClickEmailSignIn(View view){
         View vFocus = null;
         boolean cancel = false;
+        // for email processing
+        vEmail.setError(null);
+        String email = vEmail.getText().toString().trim();
+        // Check for a valid email address.
+        if (TextUtils.isEmpty(email)) {
+            vEmail.setError(getString(R.string.error_field_required));
+            vFocus = vEmail;
+            cancel = true;
+        } else if (!isEmailValid(email)) {
+            vEmail.setError(getString(R.string.error_invalid_email));
+            vFocus = vEmail;
+            cancel = true;
+        }
 
         // for password processing
         vPassword.setError(null);
@@ -66,15 +85,19 @@ public class MainActivity extends AppCompatActivity {
             // Show a progress spinner, and kick off a background task to
             // perform the user login attempt.
             showProgress(true);
-            vMessages.setText("Password:" + password);
+            vMessages.setText("Password:" + email + "." + password);
             // mAuthTask = new UserLoginTask(email, password);
             // mAuthTask.execute((Void) null);
             showProgress(false);
         }
     }
+    private boolean isEmailValid(String email) {
+        //TODO: Replace this with your own logic
+        return email.contains("@");
+    }
     private boolean isPasswordValid(String password) {
         //TODO: Replace this with your own logic
-        return password.length() > 6;
+        return password.length() >= 6;
     }
     private void showProgress(final boolean show) {
         vProgress.setVisibility(show ? View.VISIBLE : View.GONE);
